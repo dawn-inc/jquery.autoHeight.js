fdd2b7c4a74ef6fde71d25eead427bf2fa16f77d
IkutaNozomu <n.ikuta@dawn.co.jp>
2018-03-20 11:31:06 +0900

    ファイル群をコミット

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
 # jquery.autoHeight.js
+
+jQueryオブジェクトに含まれる要素のheightまたはmin-heightを最大値で揃えるプラグイン
+
+## 使用方法
+
+```js
+// 基本形
+$('.sample').autoHeight();
+```
+
+```js
+// optionsを指定する場合
+$('.sample').autoHeight({
+    minHeight: true
+});
+
+$('.sample').autoHeight({
+    column: 4
+});
+
+$('.sample').autoHeight({
+    reset: true
+});
+```
+
+```js
+// 高さ揃えを解除する場合
+var $sample = $('.sample').autoHeight();
+$sample.autoHeight('reset');
+```
+
+## オプション設定
+
+### options (object)
+
+高さ揃えに関する設定
+以下の3つをプロパティとしてもつ
+
+#### minHeight (true/false)
+
+揃える高さをmin-heightにするかどうか
+
+#### column (number)
+
+要素何個ごとに高さを揃えるか
+
+#### reset (true/false)
+
+高さを揃える前に、height(min-height)のインラインスタイルを解除するかどうか
+0または1の場合は、該当の要素すべての最大値で揃える
+
+### reset (string)
+
+引数が文字列で'reset'であるとき、すでに高さ揃えが適用されていた場合、
+該当のインラインスタイルを解除する
+
+すでに高さ揃えが適用されているかどうかは、
+jQueryオブジェクトに $.fn.autoHeight.propNameToTrack のプロパティが存在するかで判定する
+
+## デフォルト設定
+
+下記にて変更できる
+
+```js
+// optionsのデフォルト設定
+$.fn.autoHeight.defaults = {
+    column: 1,
+    reset: false,
+    minHeight: false
+}
+
+// 'reset'をかける場合に判定するプロパティ名
+$.fn.autoHeight.propNameToTrack = '_autoHeightCSSProp';
+```
+
+
+## ライセンス
+
+[MIT License](https://raw.githubusercontent.com/bhargavrpatel/gulp-prettier/master/LICENSE)
---
+++ dist/jquery.autoHeight.js
@@ -0,0 +1,126 @@
+/*!
+ * jquery.autoheight.js
+ *
+ * Author: DAWN Inc.
+ * Since:   2017-03-20
+ * Update:  -
+ * Version: 1.0.0
+ * License: MIT (http://www.opensource.org/licenses/mit-license.php, http://sourceforge.jp/projects/opensource/wiki/licenses%2FMIT_license)
+ * Comment: 右記を参考に作成(https://github.com/tinybeans/jQueryAutoHeight)
+ */
+(function($) {
+    'use strict';
+
+    /**
+     * 要素の高さを揃えるメソッド
+     * @param  {Object|String} opts オプションのオブジェクト または 'reset' という文字列
+     * @param  {Number} [1] opts.column いくつごとに高さを揃えるか。0または1の場合、すべて同じ高さに揃える
+     * @param  {Boolean} [false] opts.reset 高さを揃える前にインラインスタイルを解除するかどうか
+     * @param  {Boolean} [false] opts.minHeight min-heightで揃えるかどうか
+     * @return {jQuery} autoHeight()を適用したjQueryオブジェクト自身を返す
+     */
+    $.fn.autoHeight = function(opts) {
+        /**
+         * 変数の宣言と代入
+         */
+        var cssProp,
+            heights = [],
+            maxInRows = [],
+            propNameToTrack = $.fn.autoHeight.propNameToTrack;
+
+        if (opts === 'reset') {
+            /**
+             * 揃えていたインラインCSSプロパティを解除
+             */
+            this.css($.data(this, propNameToTrack, cssProp), '');
+
+            /**
+             * 揃えていたCSSプロパティ名のキャッシュを削除
+             */
+            $.removeData(this, propNameToTrack);
+
+            /**
+             * jQueryオブジェクトをチェーンして終了
+             */
+            return this;
+        }
+
+        /**
+         * デフォルトのオプションと、引数のオプションを統合して、実際に使用するオプションを生成
+         */
+        opts = $.extend({}, $.fn.autoHeight.defaults, opts);
+
+        /**
+         * 揃える高さのCSSプロパティ名をキャッシュ
+         */
+        cssProp = opts.minHeight ? 'minHeight' : 'height';
+
+        /**
+         * 高さを揃える前に、インラインスタイルを解除する
+         */
+        if (opts.reset) {
+            this.css(cssProp, '');
+        }
+
+        /**
+         * 各要素のheightを配列に格納
+         */
+        this.each(function(i) {
+            heights[i] = $(this).height();
+        });
+
+        if (opts.column <= 1) {
+            /**
+             * 一番大きいheightをすべての要素に適用
+             */
+            this.css(cssProp, Math.max.apply(null, heights));
+        } else {
+            /**
+             * opts.columnが2以上の場合、その数ごとに一番大きいheightを配列に格納
+             */
+            for (
+                var i = 0, j = heights.length;
+                i < Math.ceil(j / opts.column);
+                i++
+            ) {
+                var x = i * opts.column;
+
+                maxInRows.push(
+                    Math.max.apply(null, heights.slice(x, x + opts.column))
+                );
+            }
+
+            /**
+             * 各行をループ
+             */
+            for (var k = 0, l = maxInRows.length; k < l; k++) {
+                /**
+                 * 行の各要素にCSSを適用
+                 */
+                for (var m = 0; m < opts.column; m++) {
+                    this.eq(k * opts.column + m).css(cssProp, maxInRows[k]);
+                }
+            }
+        }
+
+        /**
+         * 揃えたCSSプロパティ名を、あとで`destroy`できるようにキャッシュ
+         */
+        $.data(this, propNameToTrack, cssProp);
+
+        /**
+         * jQueryオブジェクトをチェーンして終了
+         */
+        return this;
+    };
+
+    /**
+     * デフォルト設定
+     */
+    $.fn.autoHeight.propNameToTrack = '_autoHeightCSSProp';
+    $.fn.autoHeight.defaults = {
+        column: 1,
+        reset: false,
+        minHeight: false
+    };
+})(jQuery);
